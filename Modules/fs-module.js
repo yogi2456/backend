3776ad0f3818b0ee7e796134719a9cd89a6c31d0
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-04-05 12:18:36 +0530

    updates

--- Modules/fs-module.js
+++ Modules/fs-module.js
@@ -1,11 +1,14 @@
 const fs = require("fs")
 
-fs.writeFile("/hello.txt", "Welcome", function (error) {
-    if(error) console.log(error)
-    else console.log("succesfully file created")
+fs.writeFile("/hello.txt", "Welcome", (error) => {
+    if(error) {
+    console.log(error)
+    } else { 
+    console.log("succesfully file written")
+    }
 });
 
-// fs.readFile("/hello.txt", "utf8", function (err, data) {
-//     if(err) throw err;
-//     console.log(data);
-// });
+fs.readFile("/hello.txt", "utf8", function (err, data) {
+    if(err) throw err;
+    console.log(data);
+});
