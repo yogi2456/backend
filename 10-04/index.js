67d53cf720071da15e81f13aa7bd4df20a47c76a
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-04-10 10:54:37 +0530

    updates

---
+++ 10-04/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("It is Working")
+})
+
+app.post('/register', (req, res) => {
+    try {
+        // console.log(req.body, "request body")
+        const {name, email, password, confirmpassword} = req.body;
+        // console.log(name, email, password, confirmpassword, "userData")
+        if(!name || !email || !password || !confirmpassword) {
+            res.send("All fields are required")
+        } 
+        if(password !== confirmpassword) {
+            res.send("password and confirmpassword not matched")
+        }
+
+        //store data in mongodb
+        
+        res.send("Registration Succesfull")
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+app.listen(3003, () => console.log("app is running on port 3003"))
