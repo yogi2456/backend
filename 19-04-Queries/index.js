13b592c19901a17255ff2bc686e8cf9eb1bae9c8
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-04-19 11:23:00 +0530

    updates

---
+++ 19-04-Queries/index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import UserSchema from './modals/user.schema.js';
+
+
+const app = express();
+
+app.use(express.json());
+dotenv.config();
+
+app.get("/", (req, res) => {
+    res.send("It is Working..")
+});
+
+app.get("/filter-users", async (req, res) => {
+    try {
+        const { age } = req.body;
+
+        // const users = await UserSchema.find();
+        // const users = await UserSchema.find({ name: "yogesh", email: "abc@gmail.com"});
+        // const users = await UserSchema.find({age: {$eq: 20}});
+        // const users = await UserSchema.find({age: {$ne: 20}});
+        // const users = await UserSchema.find({age: {$gt: 20}});
+        // const users = await UserSchema.find({age: {$gte: 20}});
+        // const users = await UserSchema.find({age: {$lt: 30}});
+        // const users = await UserSchema.find({age: {$lte: 30}});
+        // const users = await UserSchema.find({age: {$in: [10, 20]}});
+        // const users = await UserSchema.find({age: {$nin: [10, 20]}});
+        // const users = await UserSchema.find({contact: {$exists: true}});
+        // const users = await UserSchema.find({$and: [{name: "yogesh"}, {email: "yogesh@gmail.com"}]});
+        // const users = await UserSchema.find({$or: [{name: "yogesh"}, {email: "abc@gmail.com"}]});
+        // const users = await UserSchema.find({age: {$not: {age: 20}}});
+        // const users = await UserSchema.find({$nor: [{name: "yogesh"}, {email: "yogesh@gmail.com"}]});
+        // const users = await UserSchema.find({contact: {$exists: true}});
+        // const users = await UserSchema.find({age: {$type: "20"}});
+
+        return res.status(200).json({ success: true, messsage: "users found", users});
+    } catch (error) {
+        return res.status(500).json({ success: false, error})
+    }
+})
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log("Database Connected.."));
+
+app.listen(3003, () => console.log("App is running on port 3003.."))
---
+++ 19-04-Queries/modals/user.schema.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    age: Number,
+});
+
+const UserSchema = mongoose.model("User", userSchema);
+
+export default UserSchema;
